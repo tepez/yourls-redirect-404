54ea6a9d490a395dc82d956855b57a5716d328c1
Tom Yam <tomyam1@gmail.com>
2016-12-18 13:25:38 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+yourls-redirect-404
+===================
+
+Plugin for [YOURLS](http://yourls.org) `1.5+`. 
+
+Description
+-----------
+Redirect 404 errors to a custom error page
+
+Installation
+------------
+1. In `/user/plugins`, create a new folder named `yourls-redirect-404`.
+2. Drop these files in that directory.
+3. Go to the Plugins administration page ( *eg* `http://sho.rt/admin/plugins.php` ) and activate the plugin.
+4. Have fun!
+
---
+++ plugin.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Redirect 404
+Plugin URI: https://github.com/tepez/yourls-redirect-404
+Description: Redirect 404 errors to a custom error page
+Version: 1.0
+Author: Tom Yam
+Author URI: https://github.com/tomyam1
+*/
+
+global $tp_yourls_redirect_404;
+
+/*
+* CONFIG: EDIT THIS
+*/
+
+/* The url where 404 errors are redirected to */
+$tp_yourls_redirect_404['url'] = 'https://www.tepez.co.il/error-pages/invalid-short-url';
+
+/* The QS parameter where the short url is given */
+$tp_yourls_redirect_404['qs'] = 'shortUrl';
+
+/*
+* DO NOT EDIT FARTHER
+*/
+
+yourls_add_action( 'redirect_keyword_not_found', 'tp_redirect_404' );
+yourls_add_action( 'loader_failed', 'tp_redirect_404' );
+
+function tp_redirect_404( $data ) {
+    global $tp_yourls_redirect_404;
+    $shorturl = $data[0];
+
+    $redirect_url = $tp_yourls_redirect_404['url'] .
+        '?' .
+        urlencode($tp_yourls_redirect_404['qs']) .
+        '=' .
+        urlencode($shorturl);
+
+    yourls_redirect( $redirect_url, 302 );
+    die();
+}
