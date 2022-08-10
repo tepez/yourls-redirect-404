be67f1ea55e1831e4d4f1bf87ae8213174328cfd
Tom Yam <tomyam@tepez.co.il>
2022-08-10 17:39:19 +0300

    feat: support headers added by cloudfront to detect real host, port, proto

--- plugin.php
+++ plugin.php
@@ -24,14 +24,23 @@ $tp_yourls_redirect_404['qs'] = 'shortUrl';
 * DO NOT EDIT FARTHER
 */
 
-/* Source: http://stackoverflow.com/a/8891890/1705056 */
+/**
+ * Based on http://stackoverflow.com/a/8891890/1705056
+ * With added support for headers added by CloudFront
+ **/
 function tp_url_origin( $s, $use_forwarded_host = false ) {
-    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $ssl = $s['HTTP_X_FORWARDED_PROTO'] == 'https'
+        || $s['HTTP_CLOUD_FRONT_FORWARDED_PROTO'] == 'https'
+        || $s['HTTPS'] == 'on';
     $sp       = strtolower( $s['SERVER_PROTOCOL'] );
     $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
-    $port     = $s['SERVER_PORT'];
+
+    $port     = $s['HTTP_X_FORWARDED_PORT'] || $s['SERVER_PORT'];
     $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
-    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+
+    $host     = isset( $s['HTTP_X_FORWARDED_HOST'] )
+        ? $s['HTTP_X_FORWARDED_HOST']
+        : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
     $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
     return $protocol . '://' . $host;
 }
